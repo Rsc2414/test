6911738a8637577c71ec589b5c3c179f275e608e
Rsc2414 <143814425+Rsc2414@users.noreply.github.com>
2025-07-18 21:08:48 +0530

    comit
    comit
    
    Co-Authored-By: Harsh Singh <85667181+harshsingh2414@users.noreply.github.com>

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+// MongoDB Connection 
+// coment
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
